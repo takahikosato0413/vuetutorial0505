ad2fb2fb551a7341c5d295dada3deacefa435c7b
takahikosato0413 <80609931+takahikosato0413@users.noreply.github.com>
2021-05-11 13:29:22 +0900

    ・テスト テスト

--- src/main.js
+++ src/main.js
@@ -1,98 +1,96 @@
 // https://jp.vuejs.org/v2/examples/todomvc.html
-var STORAGE_KEY = 'todos-vuejs-demo'
+var STORAGE_KEY = "todos-vuejs-demo";
 var todoStorage = {
-  fetch: function() {
-    var todos = JSON.parse(
-      localStorage.getItem(STORAGE_KEY) || '[]'
-    )
-    todos.forEach(function(todo, index) {
-      todo.id = index
-    })
-    todoStorage.uid = todos.length
-    return todos
+  fetch: function () {
+    var todos = JSON.parse(localStorage.getItem(STORAGE_KEY) || "[]");
+    todos.forEach(function (todo, index) {
+      todo.id = index;
+    });
+    todoStorage.uid = todos.length;
+    return todos;
   },
-  save: function(todos) {
-    localStorage.setItem(STORAGE_KEY, JSON.stringify(todos))
+  save: function (todos) {
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(todos));
   }
-}
+};
 
 // インスタンスの生成
+//テスト
 const app = new Vue({
-    el: '#app',
-    data: {
-      // 使用するデータ
-      todos: [],
-      options: [
-        { value: -1, label: 'すべて' },
-        { value: 0,  label: '作業中' },
-        { value: 1,  label: '完了' }
-      ],
-      // 選択している options の value を記憶するためのデータ
-      // 初期値を「-1」つまり「すべて」にする
-      current: -1
+  el: "#app",
+  data: {
+    // 使用するデータ
+    todos: [],
+    options: [
+      { value: -1, label: "すべて" },
+      { value: 0, label: "作業中" },
+      { value: 1, label: "完了" }
+    ],
+    // 選択している options の value を記憶するためのデータ
+    // 初期値を「-1」つまり「すべて」にする
+    current: -1
+  },
+  computed: {
+    computedTodos: function () {
+      // データ current が -1 ならすべて
+      // それ以外なら current と state が一致するものだけに絞り込む
+      return this.todos.filter(function (el) {
+        return this.current < 0 ? true : this.current === el.state;
+      }, this);
     },
-    computed: {
-      computedTodos: function() {
-        // データ current が -1 ならすべて
-        // それ以外なら current と state が一致するものだけに絞り込む
-        return this.todos.filter(function(el) {
-          return this.current < 0 ? true : this.current === el.state
-        }, this)
-      },
-      labels() {
-        return this.options.reduce(function(a, b) {
-          return Object.assign(a, { [b.value]: b.label })
-        }, {})
-        // キーから見つけやすいように、次のように加工したデータを作成
-        // {0: '作業中', 1: '完了', -1: 'すべて'}
+    labels() {
+      return this.options.reduce(function (a, b) {
+        return Object.assign(a, { [b.value]: b.label });
+      }, {});
+      // キーから見つけやすいように、次のように加工したデータを作成
+      // {0: '作業中', 1: '完了', -1: 'すべて'}
+    }
+  },
+  created() {
+    // インスタンス作成時に自動的に fetch() する
+    this.todos = todoStorage.fetch();
+  },
+  methods: {
+    // 使用するメソッド
+    // ToDo 追加の処理
+    doAdd: function (event, value) {
+      // ref で名前を付けておいた要素を参照
+      var comment = this.$refs.comment;
+      // 入力がなければ何もしないで return
+      if (!comment.value.length) {
+        return;
       }
+      // { 新しいID, コメント, 作業状態 }
+      // というオブジェクトを現在の todos リストへ push
+      // 作業状態「state」はデフォルト「作業中=0」で作成
+      this.todos.push({
+        id: todoStorage.uid++,
+        comment: comment.value,
+        state: 0
+      });
+      // フォーム要素を空にする
+      comment.value = "";
     },
-    created() {
-        // インスタンス作成時に自動的に fetch() する
-        this.todos = todoStorage.fetch()
-      },
-    methods: {
-      // 使用するメソッド
-          // ToDo 追加の処理
-        doAdd: function(event, value) {
-        // ref で名前を付けておいた要素を参照
-        var comment = this.$refs.comment
-        // 入力がなければ何もしないで return
-        if (!comment.value.length) {
-          return
-        }
-        // { 新しいID, コメント, 作業状態 }
-        // というオブジェクトを現在の todos リストへ push
-        // 作業状態「state」はデフォルト「作業中=0」で作成
-        this.todos.push({
-          id: todoStorage.uid++,
-          comment: comment.value,
-          state: 0
-        })
-        // フォーム要素を空にする
-        comment.value = ''
-        
-      },
-      doChangeState: function(item) {
-        item.state = item.state ? 0 : 1
-        },
-      // 削除の処理
-      doRemove: function(item) {
-        var index = this.todos.indexOf(item)
-        this.todos.splice(index, 1)
-      }
+    doChangeState: function (item) {
+      item.state = item.state ? 0 : 1;
     },
-    watch: {
-        // 監視するデータ: function(newVal, oldVal) {
-        // 変化した時に行いたい処理 
-        // オプションを使う場合はオブジェクト形式にする
-        todos: {
-        // 引数はウォッチしているプロパティの変更後の値
-        handler: function(todos) {
-            todoStorage.save(todos)
-        },
-        // deep オプションでネストしているデータも監視できる
-        deep: true
-            }
-        }
-  })
+    // 削除の処理
+    doRemove: function (item) {
+      var index = this.todos.indexOf(item);
+      this.todos.splice(index, 1);
+    }
+  },
+  watch: {
+    // 監視するデータ: function(newVal, oldVal) {
+    // 変化した時に行いたい処理
+    // オプションを使う場合はオブジェクト形式にする
+    todos: {
+      // 引数はウォッチしているプロパティの変更後の値
+      handler: function (todos) {
+        todoStorage.save(todos);
+      },
+      // deep オプションでネストしているデータも監視できる
+      deep: true
+    }
+  }
+});
